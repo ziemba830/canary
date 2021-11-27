09e0883cca0fedc2ed65490df06061595755156c
Beats-Dh <daniel15042014@hotmail.com>
2021-11-27 00:25:58 -0400

    Optimize Player::getMuteConditions()
    Optimize Player::getMuteConditions() by switching to std::vector to reduce cache misses and allocations
    
    https: //github.com/opentibiabr/optimized_forgottenserver/commit/26678f040027b0af7bde695b55f6a922e4829608
    Co-Authored-By: Jakub <38609240+SaiyansKing@users.noreply.github.com>

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -5720,8 +5720,8 @@ size_t Player::getMaxDepotItems() const {
 	return g_configManager().getNumber(FREE_DEPOT_LIMIT);
 }
 
-std::forward_list<Condition*> Player::getMuteConditions() const {
-	std::forward_list<Condition*> muteConditions;
+std::vector<Condition*> Player::getMuteConditions() const {
+	std::vector<Condition*> muteConditions;
 	for (Condition* condition : conditions) {
 		if (condition->getTicks() <= 0) {
 			continue;
@@ -5732,7 +5732,7 @@ std::forward_list<Condition*> Player::getMuteConditions() const {
 			continue;
 		}
 
-		muteConditions.push_front(condition);
+		muteConditions.push_back(condition);
 	}
 	return muteConditions;
 }
