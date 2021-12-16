40669d1724f6e84373d69a5a5476031e1dd605e8
Beats-Dh <daniel15042014@hotmail.com>
2021-12-16 12:31:34 -0400

    Fix and Optimize CreatureSay
    -Fix yelling - players underground should hear only yelling from players that are on the same floor level
    -Merge send loops with event loops since it doesn't contain any data race
    -Move received spectators vector in Game::internalCreatureSay instead of copying it
    -Use getSpectatorsInternal instead of getSpectators since it won't get any cache as it is using non-standard viewport values

--- src/creatures/monsters/monster.cpp
+++ src/creatures/monsters/monster.cpp
@@ -875,7 +875,7 @@ void Monster::doAttacking(uint32_t interval) {
 		}
 
 		if (!inRange && spellBlock.isMelee) {
-			//melee swing out of reach
+			// melee swing out of reach
 			lastMeleeAttack = 0;
 		}
 	}
